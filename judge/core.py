2feda9c8bd0431cf676fec251211d189886ecd4c
Neveralso <neveralso@gmail.com>
2014-11-16 22:06:00 +0800

    commit

--- judge/core.py
+++ judge/core.py
@@ -1,9 +1,95 @@
 # encoding: utf-8
 
+import configparser
+import logging
+import os
+import sys
 import time
 
+import pymongo
 
-def main_loop(logger):
+
+def init_logger(judge_config):
+    """
+    Init and return the logger
+    :param judge_config:
+    :return: judge_logger
+    """
+    judged_logger = logging.getLogger('Judged')
+    log_file_path = os.path.join(os.getcwd(), judge_config['log']['log_file'])
+    #check dir
+    (_path, _) = os.path.split(log_file_path)
+    if not os.path.exists(_path):
+        os.mkdir(_path)
+
+    log_handler = logging.FileHandler(log_file_path, delay=True)
+    log_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    log_handler.setFormatter(log_format)
+    log_handler.setLevel(level=logging.DEBUG)
+    judged_logger.setLevel(level=logging.DEBUG)
+    judged_logger.addHandler(log_handler)
+    return judged_logger
+
+
+def start_daemon(judge_config: configparser.ConfigParser, judge_logger: logging.Logger):
+    """
+    Start a daemon process which is running the .
+    :param judge_config:
+    :param judge_logger:
+    :return: None
+    """
+
+    pid_file_path = os.path.join(os.getcwd(), judge_config['run']['pid_file'])
+    pid = os.fork()
+    if pid > 0:
+        sys.exit(0)
+
+    os.chdir('/')
+    os.setsid()
+    os.umask(0)
+
+    pid = os.fork()
+    if pid > 0:
+        sys.exit(0)
+
+    if os.path.exists(pid_file_path):
+        print('Judged daemon is running.')
+        judge_logger.error('Judged daemon is running.')
+        exit(0)
+
+    try:
+        (_path, _) = os.path.split(pid_file_path)
+        if not os.path.exists(_path):
+            os.mkdir(_path)
+        pid_file = open(pid_file_path, mode='w+')
+        print('Judge daemon(pid=%d) start successfully.' % os.getpid())
+        judge_logger.info('Judge daemon(pid=%d) start successfully.' % os.getpid())
+        pid_file.write('%d' % os.getpid())
+        pid_file.close()
+    except Exception as e:
+        print(e)
+
+    #redirect stdio
+    sys.stdout.flush()
+    sys.stderr.flush()
+    si = open(os.devnull, 'r')
+    # so = open(os.devnull, 'a+')
+    # se = open(os.devnull, 'a+')
+    os.dup2(si.fileno(), sys.stdin.fileno())
+    # os.dup2(so.fileno(), sys.stdout.fileno())
+    # os.dup2(se.fileno(), sys.stderr.fileno())
+
+    main_loop(judge_logger)
+
+    exit(0)
+
+
+def main_loop(logger: logging.Logger):
+    """
+    A loop to check if there is
+    :param logger:
+    :return:
+    """
     while True:
         time.sleep(3)
         logger.debug('Judge daemon runs for 3s.')
--- judged.py
+++ judged.py
@@ -1,77 +1,12 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
-import os
-import sys
-import time
-import signal
 import configparser
-import logging
+import os
+import signal
+import sys
+
 import judge.core
-
-
-JudgedConfig = None
-JudgedLogger = None
-
-
-def start_daemon(pid_file_path):
-    pid = os.fork()
-    if pid > 0:
-        sys.exit(0)
-
-    os.chdir('/')
-    os.setsid()
-    os.umask(0)
-
-    pid = os.fork()
-    if pid > 0:
-        sys.exit(0)
-
-    if os.path.exists(pid_file_path):
-        print('Judged daemon is running.')
-        JudgedLogger.error('Judged daemon is running.')
-        exit(0)
-
-    try:
-        (_path, _) = os.path.split(pid_file_path)
-        if not os.path.exists(_path):
-            os.mkdir(_path)
-        pid_file = open(pid_file_path, mode='w+')
-        print('Judge daemon(pid=%d) start successfully.' % os.getpid())
-        JudgedLogger.info('Judge daemon(pid=%d) start successfully.' % os.getpid())
-        pid_file.write('%d' % os.getpid())
-        pid_file.close()
-    except Exception as e:
-        print(e)
-
-    #redirect stdio
-    sys.stdout.flush()
-    sys.stderr.flush()
-    si = open(os.devnull, 'r')
-    # so = open(os.devnull, 'a+')
-    # se = open(os.devnull, 'a+')
-    os.dup2(si.fileno(), sys.stdin.fileno())
-    # os.dup2(so.fileno(), sys.stdout.fileno())
-    # os.dup2(se.fileno(), sys.stderr.fileno())
-
-    judge.core.main_loop(JudgedLogger)
-
-
-def init_logger():
-    global JudgedLogger
-    JudgedLogger = logging.getLogger('Judged')
-    log_file_path = os.path.join(os.getcwd(), JudgedConfig['log']['log_file'])
-    #check dir
-    (_path, _) = os.path.split(log_file_path)
-    if not os.path.exists(_path):
-        os.mkdir(_path)
-
-    log_handler = logging.FileHandler(log_file_path, delay=True)
-    log_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    log_handler.setFormatter(log_format)
-    log_handler.setLevel(level=logging.DEBUG)
-    JudgedLogger.setLevel(level=logging.DEBUG)
-    JudgedLogger.addHandler(log_handler)
 
 
 def print_help():
@@ -79,17 +14,17 @@ def print_help():
 
 if __name__ == '__main__':
 
-    JudgedConfig = configparser.ConfigParser()
-    JudgedConfig.read('conf/judged.conf')
+    judge_config = configparser.ConfigParser()
+    judge_config.read('conf/judged.conf')
 
-    pid_file_path = os.path.join(os.getcwd(), JudgedConfig['run']['pid_file'])
-    init_logger()
+    pid_file_path = os.path.join(os.getcwd(), judge_config['run']['pid_file'])
+    judge_logger = judge.core.init_logger(judge_config)
 
     if len(sys.argv) < 2:
         print_help()
         exit(0)
     elif sys.argv[1] == 'start':
-        start_daemon(pid_file_path)
+        judge.core.start_daemon(judge_config, judge_logger)
     elif sys.argv[1] == 'status':
         pass
     elif sys.argv[1] == 'stop':
@@ -98,7 +33,7 @@ if __name__ == '__main__':
             pid = int(pid_file.read())
             os.kill(pid, signal.SIGKILL)
             print('Judge daemon(pid=%d) has bing killed' % pid)
-            JudgedLogger.info('Judge daemon(pid=%d) has bing killed' % pid)
+            judge_logger.info('Judge daemon(pid=%d) has bing killed' % pid)
             pid_file.close()
             os.remove(pid_file_path)
         except FileNotFoundError as e:
