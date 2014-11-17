d41d2ba3a966f258c78cb43d1f951bd435b1a118
Neveralso <neveralso@gmail.com>
2014-11-17 16:01:27 +0800

    commit

--- judge/db_adapter/mysql_adapter.py
+++ judge/db_adapter/mysql_adapter.py
@@ -8,19 +8,20 @@ from judge.db_adapter.adapter import Adapter
 
 
 class MysqlAdapter(Adapter):
-    """
-    Mysql database adapter
-    """
-    def __init__(self, host: str, port: int, database: str, user: str, password: str):
-        super().__init__()
-        try:
-            self.conn = mysql.connector.connect(
-                host=host,
-                port=port,
-                database=database,
-                user=user,
-                password=password
-            )
-        except Exception as e:
-            print(e)
+    pass
+    # """
+    # Mysql database adapter
+    # """
+    # def __init__(self, host: str, port: int, database: str, user: str, password: str):
+    #     super().__init__()
+    #     try:
+    #         self.conn = mysql.connector.connect(
+    #             host=host,
+    #             port=port,
+    #             database=database,
+    #             user=user,
+    #             password=password
+    #         )
+    #     except Exception as e:
+    #         print(e)
 
