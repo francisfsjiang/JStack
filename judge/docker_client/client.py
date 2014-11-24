70dc8f82dd5e1f9557e28e61a8954d797520fdb6
Neveralso <neveralso@gmail.com>
2014-11-24 18:09:40 +0800

    commit

--- judge/docker_client/client.py
+++ judge/docker_client/client.py
@@ -4,11 +4,11 @@ import requests
 import requests.adapters
 
 
-class Client(requests.Session):
+class DockerNode(requests.Session):
     """
 
     """
-    def __init__(self, url='unix:///var/run/docker.sock', max_container_num=10):
+    def __init__(self, url='unix:///var/run/docker.sock', max_container_num=10, docker_node_num=0):
         super().__init__()
         self.url = url
         if url.startswith('unix://'):
@@ -17,10 +17,67 @@ class Client(requests.Session):
         elif url.startswith('http://'):
             self.conn_type = 'http'
             self.host = url.strip('http://')
+        self.container_list = []
+        self.docker_node_num = docker_node_num
+        self.headers.update({'Content-Type': 'application/json'})
 
         # self.mount('http://', requests.adapters.HTTPAdapter)
 
-    def containers(self, all=True):
+    def containers(self, all_item=True):
         url = self.url + '/containers/json'
         print(url)
-        return self.get(url, params={'all': all})
+        return self.get(url, params={'all': all_item})
+
+    def create_container(self):
+        url = self.url + '/containers/create'
+
+        container_config = {
+            "Hostname": "test",
+            "Domainname": "test",
+            "User": "",
+            "Memory": 0xA00000,  #10MB=0xA00000B
+            "MemorySwap": -1,  #memory+swap
+            "CpuShares": 0,
+            "Cpuset": "",
+            "AttachStdin": False,
+            "AttachStdout": False,
+            "AttachStderr": False,
+            "Tty": False,
+            "OpenStdin": False,
+            "StdinOnce": False,
+            "Env": None,
+            "Cmd": [
+                "/bin/bash"
+            ],
+            "Entrypoint": "",
+            "Image": "ubuntu:14.10",
+            "Volumes": {
+            },
+            "WorkingDir": "",
+            "NetworkDisabled": False,
+            "ExposedPorts": {
+            },
+            "SecurityOpts": [""],
+            "HostConfig": {
+                "Binds": [],
+                "Links": [],
+                "LxcConf": {"lxc.utsname": "docker"},
+                "PortBindings": {},
+                "PublishAllPorts": False,
+                "Privileged": False,
+                "Dns": ["8.8.8.8"],
+                "DnsSearch": [""],
+                "VolumesFrom": [],
+                "CapAdd": [],
+                "CapDrop": [],
+                "RestartPolicy": {"Name": "", "MaximumRetryCount": 0},
+                "NetworkMode": "bridge",
+                "Devices": []
+            }
+        }
+        # return self.post(url, container_config, headers={'Content-Type: application/json'})
+        return self.post(url, json=container_config)
+
+    def stop_container(self):
+        url = self.url + '/containers/%s/stop?t=1' % 'eb7b8b2cfd2b'
+        return self.post(url)
