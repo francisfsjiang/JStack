c66f43cd974ad60e2c477507a45ebd784c329cc9
Neveralso <neveralso@gmail.com>
2015-01-07 11:10:17 +0800

    commit

---
+++ judged/judged/syscall_checker.c
@@ -0,0 +1,9 @@
+//
+//  syscall_checker.c
+//  judged
+//
+//  Created by Neveralso on 15/1/7.
+//  Copyright (c) 2015年 neveralso. All rights reserved.
+//
+
+#include "syscall_checker.h"
---
+++ judged/judged/syscall_checker.h
@@ -0,0 +1,14 @@
+//
+//  syscall_checker.h
+//  judged
+//
+//  Created by Neveralso on 15/1/7.
+//  Copyright (c) 2015年 neveralso. All rights reserved.
+//
+
+#ifndef __judged__syscall_checker__
+#define __judged__syscall_checker__
+
+#include <stdio.h>
+
+#endif /* defined(__judged__syscall_checker__) */
